031c81872c46c18859a7f5125dd68fd94db627d3
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-25 18:21:34 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,16 @@
 def my_select(collection)
   
   if block_given?
-  result = []
-  i = 0 
-  while i < arr.length 
-    result << yield(arr[i])
-    i += 1 
+    result = []
+    i = 0 
+    while i < arr.length 
+      if yield(arr[i]) == true 
+        result << yield(arr[i])
+      i += 1 
+    end
+    result
+  else
+    collection
   end
-  result
 
 end
