c57cbaf404a4f46bc643ebb7293ef29b80e4cc2b
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-25 19:00:49 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   result = []
     i = 0 
     while i < arr.length 
-      if yield(arr[i]) == true 
+      if yield(arr[i]) 
         result << yield(arr[i])
         i += 1 
     end
