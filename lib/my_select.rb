990c8365748623666caeacde1f98f806da63bbd8
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-25 19:02:53 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+    result = []
+    i = 0 
+    while i < arr.length 
+      if yield(arr[i]) 
+        result << arr[i]
+      end 
+      i += 1 
+    end
+    result
 end
